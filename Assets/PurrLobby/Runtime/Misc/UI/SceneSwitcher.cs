1106fbd6096ddea8a6b69ef6bc536a0fbd02c346
kevin-rus <rus.takev23@zaragoza.salesianos.edu>
2026-05-06 16:55:56 +0200

    init lobby

--- Assets/PurrLobby/Runtime/Misc/UI/SceneSwitcher.cs
+++ Assets/PurrLobby/Runtime/Misc/UI/SceneSwitcher.cs
@@ -18,7 +18,7 @@ namespace PurrLobby
     public class SceneSwitcher : MonoBehaviour
     {
         [SerializeField] private LobbyManager lobbyManager;
-        [PurrScene, SerializeField] private string nextScene;
+        [SerializeField] private string nextScene;
         [Tooltip("Automatically switch scene when OnAllReady event fires (recommended for Unity Relay)")]
         [SerializeField] private bool subscribeToOnAllReady = true;
 
